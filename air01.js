269b05f0cc95084deffc888617b927be8040e005
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-06-07 20:26:09 +0200

    🌬️  Changes in the first algorithm and done with the second one

--- air01.js
+++ air01.js
@@ -1,18 +1,17 @@
-// Split
+// Split en fonction
 /*
-Créez un programme qui découpe une chaîne de caractères en tableau (séparateurs : espaces, tabulations, retours à la ligne).
+Créez un programme qui découpe une chaîne de caractères en tableau en fonction du séparateur donné en 2e argument.
 
-Votre programme devra utiliser une fonction prototypée comme ceci :
+Votre programme devra intégrer une fonction prototypée comme ceci :
 ma_fonction(string_à_couper, string_séparateur) { // syntaxe selon votre langage
 	# votre algorithme
 	return (tableau)
 }
 
 Exemples d’utilisation :
-$> python exo.py “Bonjour les gars”
-Bonjour
-les
-gars
+$> python exo.py “Crevette magique dans la mer des étoiles” “la”
+Crevette magique dans 
+ mer des étoiles
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 */
@@ -22,36 +21,33 @@ const printError = (errorMessage) => {
   console.error(errorMessage);
 };
 const handlingErrorsInArguments = (arguments) => {
-  const userInput = arguments[2];
-  if (arguments.length !== 3) {
+  const stringToSplit = arguments[2];
+  if (arguments.length !== 4) {
     printError(
-      "Error. Not enough or too much arguments. Please enter only one string in arguments"
+      'Error. Not enough or too much arguments. Please enter strings in arguments. Usage : node air01.js "String to split" "Splitter" '
     );
     process.exit(0);
   }
-  if (!isNaN(userInput)) {
+  if (!isNaN(stringToSplit)) {
     printError("Error. The string you entered contains only numbers");
     process.exit(0);
   }
 };
-
-const isSpaceTabOrNewLine = (char) => {
-  return char === " " || char === "\n" || char === "\t";
-};
-
-const splitText = (stringToSplit) => {
-  let splittedWords = [];
+const splitText = (stringToSplit, splitter) => {
+  const splittedWords = [];
   let lastSliceIndex = 0;
+
   for (let i = 0; i < stringToSplit.length; i++) {
-    if (isSpaceTabOrNewLine(stringToSplit[i])) {
+    if (stringToSplit.startsWith(splitter, i)) {
       splittedWords.push(stringToSplit.slice(lastSliceIndex, i));
-      lastSliceIndex = i;
+      lastSliceIndex = i + splitter.length;
+      i += splitter.length - 1;
     }
   }
-  splittedWords.push(stringToSplit.slice(lastSliceIndex));
-  return splittedWords.map((element) => element.replace(/ /g, ""));
-};
 
+  splittedWords.push(stringToSplit.slice(lastSliceIndex));
+  return splittedWords;
+};
 const printArray = (array) => {
   for (element of array) {
     console.log(element);
@@ -60,9 +56,10 @@ const printArray = (array) => {
 // Handling errors
 handlingErrorsInArguments(args);
 // Parsing
-const userInput = args[2];
+const stringToSplit = args[2];
+const splitter = args[3];
 // Solving problem
-const result = splitText(userInput);
+const result = splitText(stringToSplit, splitter);
 
 // Printing results
 printArray(result);
